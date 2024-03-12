cc9be58eb45db478d6d2639aacd9035a10de063c
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2024-03-13 01:04:18 +0700

    update content

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ const app = express();
 // Enable CORS for all routes
 app.use(cors());
 
-app.get("/schedule", async (req, res) => {
+app.get("/Student.aspx", async (req, res) => {
     const cookie = req.headers.fapcookie;
 
     try {
